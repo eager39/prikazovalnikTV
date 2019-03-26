6debea03b8c73c708659dd8eb87014d4591fa40c
eager39 <xxxzanxxx@gmail.com>
2019-03-26 21:35:39 +0100

    bazta

--- baza.sql
+++ baza.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `displaytv` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `displaytv`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: displaytv
+-- Host: localhost    Database: displaytv
 -- ------------------------------------------------------
--- Server version	5.5.5-10.1.37-MariaDB
+-- Server version	5.5.5-10.1.9-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,7 +27,7 @@ CREATE TABLE `displays` (
   `name` varchar(45) DEFAULT NULL,
   `location` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,8 +36,34 @@ CREATE TABLE `displays` (
 
 LOCK TABLES `displays` WRITE;
 /*!40000 ALTER TABLE `displays` DISABLE KEYS */;
-INSERT INTO `displays` VALUES (9,'1','zunaj');
+INSERT INTO `displays` VALUES (13,'test','test'),(14,'test2','test2');
 /*!40000 ALTER TABLE `displays` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `graphs`
+--
+
+DROP TABLE IF EXISTS `graphs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `graphs` (
+  `id_graph` int(11) NOT NULL AUTO_INCREMENT,
+  `data` varchar(500) DEFAULT NULL,
+  `columns` varchar(500) DEFAULT NULL,
+  `name_graph` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_graph`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `graphs`
+--
+
+LOCK TABLES `graphs` WRITE;
+/*!40000 ALTER TABLE `graphs` DISABLE KEYS */;
+INSERT INTO `graphs` VALUES (14,'[[\"jan\",2010],[\"feb\",2011],[\"mar\",2012],[\"apr\",2013],[\"may\",2014],[\"jun\",2015],[\"jul\",2016],[\"aug\",2017]]','[\"jan\",\"feb\",\"mar\",\"apr\",\"may\",\"jun\",\"jul\",\"aug\"]','qqqqqqqqq'),(15,'[[\"jan\",2010],[\"feb\",2011],[\"mar\",2012],[\"apr\",2013],[\"may\",2014],[\"jun\",2015],[\"jul\",2016],[\"aug\",2017]]','[\"jan\",\"feb\",\"mar\",\"apr\",\"may\",\"jun\",\"jul\",\"aug\"]','aaaaaaaaaaa');
+/*!40000 ALTER TABLE `graphs` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -55,12 +79,13 @@ CREATE TABLE `items` (
   `type` varchar(45) NOT NULL,
   `active` int(11) NOT NULL,
   `ord` int(11) DEFAULT '0',
-  `display` int(11) NOT NULL,
+  `display` int(30) NOT NULL,
   `duration` varchar(45) DEFAULT '5000',
+  `graph` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_items_displays_idx` (`display`),
-  CONSTRAINT `fk_items_displays` FOREIGN KEY (`display`) REFERENCES `displays` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8;
+  KEY `fk_items_graphs1_idx` (`graph`),
+  CONSTRAINT `fk_items_graphs1` FOREIGN KEY (`graph`) REFERENCES `graphs` (`id_graph`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=172 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,9 +94,35 @@ CREATE TABLE `items` (
 
 LOCK TABLES `items` WRITE;
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
-INSERT INTO `items` VALUES (67,'{\"naslov\":\"qqqqqq\",\"vsebina\":\"qqqqq\"}','text',1,3,9,'2000'),(68,'SampleVideo_1280x720_5mb.mp4','video',1,6,9,'2000'),(69,'PDF TEST.pdf','pdf',1,1,9,'2000'),(70,'pdf-test.pdf','pdf',1,2,9,'2000'),(80,'{\"naslov\":\"test\",\"vsebina\":\"test\"}','text',1,4,9,'2000');
+INSERT INTO `items` VALUES (165,'qqqqqqqqq','graph',1,0,13,'5000',14),(166,'aaaaaaaaaaa','graph',1,1,13,'5000',15),(169,'qqqqqqqqq','graph',1,0,14,'5000',14),(170,'aaaaaaaaaaa','graph',1,2,14,'5000',15),(171,'aaaaaaaaaaa','graph',1,3,14,'5000',15);
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ALLOW_INVALID_DATES,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `displaytv`.`items_AFTER_DELETE`
+AFTER DELETE ON `displaytv`.`items`
+FOR EACH ROW
+BEGIN
+DECLARE updatecount INT;
+  set updatecount = ( select count(*) from items where graph = old.graph );
+  if updatecount=0
+    then
+     DELETE FROM graphs WHERE id_graph=old.graph;
+  end if;
+
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -82,4 +133,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-14 15:19:45
+-- Dump completed on 2019-03-26 21:32:33
